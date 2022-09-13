b1c54bdb96e4101ee641092634f7ae9a39de5d78
guihi1 <gui.hikio@gmail.com>
2022-09-13 19:46:40 -0300

    Create gameEnd function

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const board = document.querySelector(".board");
+const message = document.querySelector(".game-end");
+const p = document.createElement("p");
+
+message.appendChild(p);
 
 const gameBoard = ((board) => {
     const squareArray = [];
@@ -23,12 +27,47 @@ const displayController = (() => {
     for(let i = 0; i < 9; i++){
         gameBoard.squareArray[i].addEventListener("click", () => {
             if(turn == "player1"){
-                gameBoard.squareArray[i].textContent = "X";
+                gameBoard.squareArray[i].textContent = player1.choice;
+                gameBoard.squareArray[i].style.pointerEvents = "none";
                 turn = "player2";
+                gameEnd.check();
             } else if(turn == "player2"){
-                gameBoard.squareArray[i].textContent = "O";
+                gameBoard.squareArray[i].textContent = player2.choice;
+                gameBoard.squareArray[i].style.pointerEvents = "none";
                 turn = "player1";
+                gameEnd.check();
             }
         });
     }
+})();
+
+const gameEnd = (() => {
+    function disableMove() {
+        for(let i = 0; i < 9; i++){
+            gameBoard.squareArray[i].style.pointerEvents = "none";
+        }
+    };
+    const check = () => {
+        for(let i = 0; i < 7; i += 3){
+            if(gameBoard.squareArray[i].textContent === gameBoard.squareArray[i+1].textContent && gameBoard.squareArray[i].textContent === gameBoard.squareArray[i+2].textContent && gameBoard.squareArray[i].textContent != ""){
+                p.textContent = "Player 1 won!";
+                disableMove();
+            }
+        }
+        for(let i = 0; i < 3; i++){
+            if(gameBoard.squareArray[i].textContent === gameBoard.squareArray[i+3].textContent && gameBoard.squareArray[i].textContent === gameBoard.squareArray[i+6].textContent && gameBoard.squareArray[i].textContent != ""){
+                p.textContent = "Player 1 won!";
+                disableMove();
+            }
+        }
+        if(gameBoard.squareArray[0].textContent === gameBoard.squareArray[4].textContent && gameBoard.squareArray[0].textContent === gameBoard.squareArray[8].textContent && gameBoard.squareArray[0].textContent != ""){
+            p.textContent = "Player 1 won!";
+            disableMove();
+        }
+        if(gameBoard.squareArray[2].textContent === gameBoard.squareArray[4].textContent && gameBoard.squareArray[2].textContent === gameBoard.squareArray[6].textContent && gameBoard.squareArray[2].textContent != ""){
+            p.textContent = "Player 1 won!";
+            disableMove();
+        }
+    };
+    return {check};
 })();
